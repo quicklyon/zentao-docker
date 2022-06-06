fb9ec8450a4bf6820ac9d8f42155da3ef1df2d83
周悦秋 <zhouyueqiu@easycorp.ltd>
2022-06-06 14:57:29 +0800

    * fix post install check script bug

--- debian/rootfs/usr/bin/check_files.sh
+++ debian/rootfs/usr/bin/check_files.sh
@@ -13,7 +13,7 @@ do
                     -u"$MYSQL_USER" \
                     -p"$MYSQL_PASSWORD" \
                     -D"$MYSQL_DB" \
-                    -e "select * from \`${table_prefix}_config\` where \`key\`='version';" 2>/dev/null | wc -l)
+                    -e "select * from \`${table_prefix}config\` where \`key\`='version';" 2>/dev/null | wc -l)
     fi
 
     if [ "$DB_INITED" != "0" ] && [ "$CFG_INITED" != "0" ];then

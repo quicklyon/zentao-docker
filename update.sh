314999922c4f68d66b8a6af4db6cf001b2eb7955
zhouyq <zhouyueqiu@easycorp.ltd>
2022-08-31 13:32:04 +0800

    * Update zentao 17.6 ,7.6 , 3.6.1

--- update.sh
+++ update.sh
@@ -10,21 +10,22 @@ VERSION_URL="https://www.zentao.net/download.html"
 HTML_CONTENT=$(curl -sL ${VERSION_URL} | htmlq --attribute href a)
 
 REGULAR=(
-    "zentaopms[[:digit:]]+.[[:digit:]]+"
-    "biz[[:digit:]]+.[[:digit:]]+"
-    "max[[:digit:]]+.[[:digit:]]+"
-    "litev[[:digit:]]+.[[:digit:]]+"
-    "litevipv[[:digit:]]+.[[:digit:]]+"
+    "zentaopms[[:digit:]]+(\.[[:digit:]]+)+"
+    "biz[[:digit:]]+(\.[[:digit:]]+)+"
+    "max[[:digit:]]+(\.[[:digit:]]+)+"
+    "litev[[:digit:]]+\.[[:digit:]]+"
+    "litevipv[[:digit:]]+\.[[:digit:]]+"
 )
 
 get_latest_ver(){
     local regular=${1:?regular is error!}
-    grep -Eo "$regular" <<< "$HTML_CONTENT" | sort -nr | head -n 1 
+    local key=${2:?key is error}
+    grep -Eo "$regular" <<< "$HTML_CONTENT" | sed "s/${key}//g" | sort -nur | head -n 1 
 }
 
 get_news_url(){
     local regular=${1:?regular is error!}
-    url=$(grep -E "$regular" <<< "$HTML_CONTENT" | sort -nr | head -n 1 )
+    url=$(grep -E "$regular" <<< "$HTML_CONTENT" | sort -u | head -n 1 )
     echo "https://www.zentao.net$url"
 }
 
@@ -40,7 +41,7 @@ do
     key="${release//\[\[*/}"
 
     # get last version
-    last_ver="$(get_latest_ver "$release" | sed "s/${key}//g")"
+    last_ver="$(get_latest_ver "$release" "$key")"
 
     # have not version info
     if [ -z "$last_ver" ];then
@@ -62,7 +63,7 @@ CUR_MD5=$(md5sum version.json |awk '{print $1}')
 
 if [ "$LAST_MD5" != "$CUR_MD5" ];then
     warn "New version of ZenTao detected!"
-    diff --color=always -u1 <(echo "$JSON") <(cat version.json)
+    diff --color=always -u1 <(cat version.json) <(echo "$JSON")
     jq <<< "$JSON" > version.json
 else
     info "ZenTao all versions are the latest."

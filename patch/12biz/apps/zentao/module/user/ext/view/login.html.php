97ccca02c43a22bd14e9e13e618f01ec60f0f9bf
zhouyq <zhouyueqiu@easycorp.ltd>
2023-05-05 14:02:47 +0800

    Add zentao 12 version

---
+++ patch/12biz/apps/zentao/module/user/ext/view/login.html.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The html template file of login method of user module of ZenTaoPMS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     ZenTaoPMS
+ * @version     $Id: login.html.php 5084 2013-07-10 01:31:38Z wyd621@gmail.com $
+ */
+include '../../../common/view/header.lite.html.php';
+if(empty($config->notMd5Pwd))js::import($jsRoot . 'md5.js');
+?>
+<main id="main" class="fade">
+  <div class="container" id="login">
+    <div id="loginPanel">
+      <header>
+        <h2><?php printf($lang->welcome, $app->company->name);?></h2>
+        <div class="actions dropdown dropdown-hover" id='langs'>
+          <button type='button' class='btn' title='Change Language/更换语言/更換語言'><?php echo $config->langs[$this->app->getClientLang()]; ?> <span class="caret"></span></button>
+          <ul class="dropdown-menu pull-right">
+            <?php foreach($config->langs as $key => $value):?>
+            <li><a class="switch-lang" data-value="<?php echo $key; ?>"><?php echo $value; ?></a></li>
+            <?php endforeach;?>
+          </ul>
+        </div>
+      </header>
+      <div class="table-row">
+        <div class="col-4 text-center text-middle">
+        <img src="<?php echo $config->webRoot . 'theme/default/images/main/zt-logo.png';?>" alt="">
+        </div>
+        <div class="col-8">
+          <form method='post' target='hiddenwin'>
+            <table class='table table-form'>
+              <tbody>
+                <tr>
+                  <th><?php echo $lang->user->account;?></th>
+                  <td><input class='form-control' type='text' name='account' id='account' value='demo' autofocus /></td>
+                </tr>
+                <tr>
+                  <th><?php echo $lang->user->password;?></th>
+                  <td><input class='form-control' type='password' name='password' value='quickon4You' /></td>
+                </tr>
+                <tr>
+                  <th></th>
+                  <td id="keeplogin"><?php echo html::checkBox('keepLogin', $lang->user->keepLogin, $keepLogin);?></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td class="form-actions">
+                  <?php 
+                  echo html::submitButton($lang->login, '', 'btn btn-primary');
+                  if($app->company->guest) echo html::linkButton($lang->user->asGuest, $this->createLink($config->default->module));
+                  echo html::hidden('referer', $referer);
+                  echo html::a(inlink('reset'), $lang->user->resetPassword);
+                  ?>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
+      </div>
+      <?php if(isset($demoUsers)):?>  
+      <footer>
+        <span><?php echo $lang->user->loginWithDemoUser;?></span>
+        <?php
+        $password = md5('quickon4You');
+        $link     = inlink('login');
+        $link    .= strpos($link, '?') !== false ? '&' : '?';
+        foreach($demoUsers as $demoAccount => $demoUser)
+        {
+            if($demoUser->password != $password) continue;
+            echo html::a($link . "account={$demoAccount}&password=" . md5($password . $this->session->rand), $demoUser->realname);
+        }
+        ?>  
+      </footer>  
+      <?php endif;?>
+    </div>
+
+  <p style='margin-top:25px; color:#fff; text-align:center'>
+    版本选择：
+    <a href='http://zentao12.demo.haogs.cn' style='color:yellow'>开源版</a>
+  </p>
+    <div id="info" class="table-row">
+      <div class="table-col text-middle text-center">
+        <div id="poweredby">
+          <?php if($config->checkVersion):?>
+          <?php $siteURL = $this->app->getClientLang() == 'en' ? 'http://api.zentao.pm' : 'http://api.zentao.net';?>
+          <iframe id='updater' class='hidden' frameborder='0' width='100%' scrolling='no' allowtransparency='true' src="<?php echo $siteURL;?>/updater-isLatest-<?php echo $config->version;?>-<?php echo $s;?>.html?lang=<?php echo str_replace('-', '_', $this->app->getClientLang())?>"></iframe>
+          <?php endif;?>
+          <?php echo html::hidden('verifyRand', $rand);?>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php include '../../../common/view/footer.lite.html.php';?>

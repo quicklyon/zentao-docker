7311675bcade14f9a4f99e984ccf00baf1b1a493
zhouyueqiu <zhouyueqiu@easycorp.ltd>
2023-08-31 15:05:02 +0800

    * Fix mysql args for sentry.

--- debian/rootfs/opt/easysoft/scripts/libquery.sh
+++ debian/rootfs/opt/easysoft/scripts/libquery.sh
@@ -9,7 +9,7 @@
 
 [ -n "${DEBUG:+1}" ] && set -x
 
-MYSQL_BIN="/opt/zbox/bin/mysql -uroot -p$ZT_MYSQL_PASSWORD -D$ZT_MYSQL_DB"
+MYSQL_BIN="/opt/zbox/bin/mysql -u$ZT_MYSQL_USER -p$ZT_MYSQL_PASSWORD -h$ZT_MYSQL_HOST -P$ZT_MYSQL_PORT -D$ZT_MYSQL_DB"
 export MYSQL_BIN
 
 # 获取当前运行的版本

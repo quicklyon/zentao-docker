6b5f8cb8aaefc0df767b90f1048d52bf33011fe5
zhouyueqiu <zhouyueqiu@easycorp.ltd>
2023-12-01 16:26:15 +0800

    - Remove config/ext process logic from sentry.

--- test/scripts/check-apache-domain-subdir.sh
+++ test/scripts/check-apache-domain-subdir.sh
@@ -6,6 +6,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ "$APP_WEB_ROOT" != "" ];then
+if [ "${APP_WEB_ROOT:-}" != "" ];then
     [[ $(grep -c "$APP_WEB_ROOT" /opt/zbox/etc/apache/httpd.conf ) -eq 2 ]] || exit 1
 fi
--- test/scripts/check-internal-mysql.sh
+++ test/scripts/check-internal-mysql.sh
@@ -7,7 +7,7 @@ set -o nounset
 set -o pipefail
 
 # 启用内部数据库，并且没有指定外部数据库地址
-if [ "$MYSQL_INTERNAL" == "true" ] && [ "$ZT_MYSQL_HOST" == "127.0.0.1" ];then
+if [ "${MYSQL_INTERNAL:-}" == "true" ] && [ "${ZT_MYSQL_HOST:-}" == "" ];then
 
     # 检查是否设置了mysql启动脚本
     [ -L /etc/s6/s6-enable/00-mysql ] || exit 1
